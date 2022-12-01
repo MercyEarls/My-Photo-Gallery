36bc556f1c736c52a054d7b32e2f2b091aa5baec
MercyEarls <114352112+MercyEarls@users.noreply.github.com>
2022-12-01 16:45:15 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,151 @@
+// row1first
+
+let r1div1 = document.getElementById('row1first')
+let r1btn1 = document.getElementById('r1b1')
+function doAChanger1d1(){
+    // alert('We wanna do a change')
+    r1div1.style.backgroundColor = 'red'
+    r1btn1.style.backgroundColor = 'grey'
+}
+
+function doBChanger1d1(){
+    r1div1.style.backgroundColor = 'green'
+}
+
+function doCChanger1d1(){
+    r1div1.style.backgroundColor = 'plum'
+}
+
+// row1second
+let r1div2 = document.getElementById('row1second')
+let r1btn2 = document.getElementById('r1b2')
+function doAChanger1d2(){
+    // alert('We wanna do a change')
+    r1div2.style.backgroundColor = 'red'
+    r1btn2.style.backgroundColor = 'grey'
+}
+
+function doBChanger1d2(){
+    r1div2.style.backgroundColor = 'green'
+}
+
+function doCChanger1d2(){
+    r1div2.style.backgroundColor = 'plum'
+}
+
+// row1third
+let r1div3 = document.getElementById('row1third')
+let r1btn3 = document.getElementById('r1b3')
+function doAChanger1d3(){
+    // alert('We wanna do a change')
+    r1div3.style.backgroundColor = 'red'
+    r1btn3.style.backgroundColor = 'grey'
+}
+
+function doBChanger1d3(){
+    r1div3.style.backgroundColor = 'green'
+}
+
+function doCChanger1d3(){
+    r1div3.style.backgroundColor = 'plum'
+}
+
+// secondrow
+// row2div1
+let r2div1 = document.getElementById('row2first')
+let r2btn1 = document.getElementById('r2b1')
+function doAChanger2d1(){
+    // alert('We wanna do a change')
+    r2div1.style.backgroundColor = 'red'
+    r2btn1.style.backgroundColor = 'grey'
+}
+
+function doBChanger2d1(){
+    r2div1.style.backgroundColor = 'green'
+}
+
+function doCChanger2d1(){
+    r2div1.style.backgroundColor = 'plum'
+}
+// row2div2
+let r2div2 = document.getElementById('row2second')
+let r2btn2 = document.getElementById('r2b2')
+function doAChanger2d2(){
+    // alert('We wanna do a change')
+    r2div2.style.backgroundColor = 'red'
+    r2btn2.style.backgroundColor = 'grey'
+}
+
+function doBChanger2d2(){
+    r2div2.style.backgroundColor = 'green'
+}
+
+function doCChanger2d2(){
+    r2div2.style.backgroundColor = 'plum'
+}
+
+// row2div3
+let r2div3 = document.getElementById('row2third')
+let r2btn3 = document.getElementById('r2b3')
+function doAChanger2d3(){
+    // alert('We wanna do a change')
+    r2div3.style.backgroundColor = 'red'
+    r2btn3.style.backgroundColor = 'grey'
+}
+
+function doBChanger2d3(){
+    r2div3.style.backgroundColor = 'green'
+}
+
+function doCChanger2d3(){
+    r2div3.style.backgroundColor = 'plum'
+}
+// row3div1
+let r3div1 = document.getElementById('row3first')
+let r3btn1 = document.getElementById('r3b1')
+function doAChanger3d1(){
+    // alert('We wanna do a change')
+    r3div1.style.backgroundColor = 'red'
+    r3btn1.style.backgroundColor = 'grey'
+}
+
+function doBChanger3d1(){
+    r3div1.style.backgroundColor = 'green'
+}
+
+function doCChanger3d1(){
+    r3div1.style.backgroundColor = 'plum'
+}
+// row3div2
+let r3div2 = document.getElementById('row3second')
+let r3btn2 = document.getElementById('r3b2')
+function doAChanger3d2(){
+    // alert('We wanna do a change')
+    r3div2.style.backgroundColor = 'red'
+    r3btn2.style.backgroundColor = 'grey'
+}
+
+function doBChanger3d2(){
+    r3div2.style.backgroundColor = 'green'
+}
+
+function doCChanger3d2(){
+    r3div2.style.backgroundColor = 'plum'
+}
+// row3div3
+let r3div3 = document.getElementById('row3third')
+let r3btn3 = document.getElementById('r3b3')
+function doAChanger3d3(){
+    // alert('We wanna do a change')
+    r3div3.style.backgroundColor = 'red'
+    r3btn3.style.backgroundColor = 'grey'
+}
+
+function doBChanger3d3(){
+    r3div3.style.backgroundColor = 'green'
+}
+
+function doCChanger3d3(){
+    r3div3.style.backgroundColor = 'plum'
+
+}
